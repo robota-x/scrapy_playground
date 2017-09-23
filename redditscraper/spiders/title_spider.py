c09c7ce212043144362ad8af51d8fc850261d393
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2017-09-23 12:42:19 +0100

    build comment crawler skeleton

--- redditscraper/spiders/title_spider.py
+++ redditscraper/spiders/title_spider.py
@@ -18,3 +18,26 @@ class TitleSpider(scrapy.Spider):
                 'text': title.xpath('.//a/text()').extract_first(),
                 'url': title.xpath('.//a/@href').extract_first()
             }
+
+
+class CommentSpider(scrapy.Spider):
+    name = 'comments'
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url='https://reddit.com',
+            callback=self.parse_top_level
+        )
+
+    def parse_top_level(self, response):
+        selector_string = '//ul[contains(@class, "flat-list")]/li/a/@href'
+        comment_link_list = response.xpath(selector_string).extract()
+
+        for comment_link in comment_link_list:
+            yield response.follow(
+                url=comment_link,
+                callback=self.parse_comments
+            )
+
+    def parse_comments(self, response):
+        pass
