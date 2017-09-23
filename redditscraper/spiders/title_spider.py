5ab5c86751d01eeb0aeeafe4460ceef46f5706c1
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2017-09-23 14:35:00 +0100

    complete scraper too yield all comment content as list of dom elements

--- redditscraper/spiders/title_spider.py
+++ redditscraper/spiders/title_spider.py
@@ -30,8 +30,8 @@ class CommentSpider(scrapy.Spider):
         )
 
     def parse_top_level(self, response):
-        selector_string = '//ul[contains(@class, "flat-list")]/li/a/@href'
-        comment_link_list = response.xpath(selector_string).extract()
+        link_selector = '//ul[contains(@class, "flat-list")]/li/a/@href'
+        comment_link_list = response.xpath(link_selector).extract()
 
         for comment_link in comment_link_list:
             yield response.follow(
@@ -40,4 +40,11 @@ class CommentSpider(scrapy.Spider):
             )
 
     def parse_comments(self, response):
-        pass
+        comment_selector = '//div[@class="commentarea"]//div[contains(@class, "usertext-body")]/div[@class="md"]'
+        comment_list = response.xpath(comment_selector)
+
+        for comment_text in comment_list:
+            yield {
+                # extract all elements as a list
+                'text': comment_text.xpath('*').extract()
+            }
